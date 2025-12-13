2fcf7b1e520a8ac139f05cc0571f96f7cc1f1925
Klezya <ca.kupfer@duocuc.cl>
2025-12-13 16:29:45 -0300

    Inicializacion

---
+++ src/test/java/com/pokestore/poke_api/PokeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokestore.poke_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
